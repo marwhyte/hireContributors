41fc789944b75e63ae189f2590adefef77304486
Marco Whyte <mwhyte@umass.edu>
2020-09-13 23:30:09 -0500

    success before warning

--- packagejsonemployer/src/functions/copyEmail.ts
+++ packagejsonemployer/src/functions/copyEmail.ts
@@ -4,6 +4,15 @@ export const copyEmail = (canidateName: string, canidatePackage: string) => {
   var email = localStorage.getItem("emailTemplate");
   if (email === null || email === undefined) {
     const defaultString = `Hello ${canidateName},\n I noticed your work at ${canidatePackage} and was impressed, would you be interested in having a conversion about possible careers? If so, reach out!\nSincerely`;
+    toast.success("📧Template email with canidate info copied to clipboard", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
     toast.warning(
       "⚠️ Using Default Email Template, it is recommended to create your own Template!",
       {
@@ -16,15 +25,6 @@ export const copyEmail = (canidateName: string, canidatePackage: string) => {
         progress: undefined,
       }
     );
-    toast.success("📧Template email with canidate info copied to clipboard", {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
     navigator.clipboard.writeText(defaultString);
   } else {
     const packageReplace = email.replace(/{{PACKAGENAME}}/g, canidatePackage);
